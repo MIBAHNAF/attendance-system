527caaf48eae236fc7ff11ed55802df6e56c63ad
Mir Ahnaf Ali <mirahnafali717@gmail.com>
2025-02-01 03:44:07 -0500

    progress

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
-import supabase from "../../lib/supabase";
 
 export default function Dashboard() {
     const router = useRouter();
